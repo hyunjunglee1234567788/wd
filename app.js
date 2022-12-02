353cc502bf27a5e55eff38a30299b2e20be02049
leeh <hjja1324@gmail.com>
2022-12-02 17:33:38 +0900

    winston中

--- app.js
+++ app.js
@@ -3,12 +3,18 @@
 const express = require("express");
 const bodyparser =require ("body-parser");
 const dotenv = require("dotenv");
-dotenv.config();
+const morgan = require("morgan");
+
 const app = express();
+dotenv.config();
 // router 
 const home = require("./src/routes/home");
 //PORT 
 const PORT = 3000;
+
+const logger = require("./src/config/logger");
+logger.log("info","hi");
+
 
 // app setting (contect ejs)
 app.set("views",("./src/views"));
@@ -16,6 +22,7 @@ app.set("view engine","ejs");
 app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:true}));
+app.use(morgan("dev"));
 // middle wear 
 app.use("/",home);
 
